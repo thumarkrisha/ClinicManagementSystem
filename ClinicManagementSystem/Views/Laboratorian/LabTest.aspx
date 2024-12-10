3e6047702346100a5bb8b0a62a63e6bc72656e15
thumarkrisha <thumakrisha2308@gmail.com>
2024-12-10 15:25:10 +0530

    frontend+backend of LabTest page in laboratorian module

---
+++ ClinicManagementSystem/Views/Laboratorian/LabTest.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Laboratorian/LaboratorianMaster.Master" AutoEventWireup="true" CodeBehind="LabTest.aspx.cs" Inherits="ClinicManagementSystem.Views.Laboratorian.LabTest" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Mybody" runat="server">
+        <div class="container-fluid">
+            <div class="row" style="height:50px"></div>
+    <div class="row">
+         <div class="col-md-4">
+             <h2>Laboratory Test Detail</h2>
+             <form>
+              <div class="mb-3">
+                <label for="LabNameTb" class="form-label">Name</label>
+                <input type="text" class="form-control" id="TestNameTb" runat="server"/>
+              </div>
+
+              <div class="mb-3">
+                <label for="TestCostTb" class="form-label">Test Cost</label>
+                <input type="text" class="form-control" id="TestCostTb" runat="server"/>
+              </div>
+
+            <div class="row" style="height:50px"></div>
+            
+
+               <asp:Label ID="ErrMsg" runat="server" class="text-danger"></asp:Label><br />
+                 <asp:Button ID="EditBtn" runat="server" class="btn btn-warning" Text=" Edit " OnClick="EditBtn_Click" ></asp:Button>
+                 <asp:Button ID="SaveBtn" runat="server" class="btn btn-primary" Text=" Save " OnClick="SaveBtn_Click" ></asp:Button>
+                 <asp:Button ID="DeleteBtn" runat="server" class="btn btn-danger" Text=" Delete " OnClick="DeleteBtn_Click"  ></asp:Button>
+              
+            </form>
+         </div>
+         <div class="col-md-8">
+             <div class="row">
+                 <div class="col">
+                     <img src="../../Assets/Images/lab.png" width="100%" height="300px" class="rounded-3"/>
+                 </div>
+             </div>
+             <div class="row">
+                  <div class="col">
+                     <h1>Laborator Test Details</h1>
+                      <asp:GridView ID="LabTestGV" class="table table-hover" runat="server" AutoGenerateSelectButton="True" CellPadding="4" ForeColor="#333333" GridLines="None" OnSelectedIndexChanged="LabTestGV_SelectedIndexChanged" >
+                          <AlternatingRowStyle BackColor="White" />
+                          <EditRowStyle BackColor="#2461BF" />
+                          <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                          <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                          <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                          <RowStyle BackColor="#EFF3FB" />
+                          <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                          <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                          <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                          <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                          <SortedDescendingHeaderStyle BackColor="#4870BE" />
+                      </asp:GridView>
+                 </div>
+             </div>
+         </div>
+    </div>
+
+</div>
+</asp:Content>
---
+++ ClinicManagementSystem/Views/Laboratorian/LabTest.aspx.cs
@@ -0,0 +1,118 @@
+﻿using ClinicManagementSystem.Views.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ClinicManagementSystem.Views.Laboratorian
+{
+    public partial class LabTest : System.Web.UI.Page
+    {
+        Models.Functions con;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con = new Models.Functions();
+            showTests();
+        }
+        private void showTests()
+        {
+            string query = "select * from LabTestTbl";
+            LabTestGV.DataSource = con.GetDatas(query);
+            LabTestGV.DataBind();
+        }
+        protected void SaveBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string TestName = TestNameTb.Value.ToString();
+                string TestCost = TestCostTb.Value.ToString();
+              
+                string Query = "insert into LabTestTbl values('{0}','{1}',{2})";
+                Query = string.Format(Query, TestName,TestCost,3);
+
+                con.SetDatas(Query);
+                ErrMsg.Text = "Test Added!!!";
+                showTests();
+                TestNameTb.Value = "";
+                TestCostTb.Value = "";
+
+            }
+            catch (Exception ex)
+            {
+                ErrMsg.Text = ex.Message;
+            }
+        }
+        int key = 0;
+        protected void LabTestGV_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            TestNameTb.Value = LabTestGV.SelectedRow.Cells[2].Text;
+            TestCostTb.Value = LabTestGV.SelectedRow.Cells[3].Text;
+           
+            //Response.Write(GenderCb.SelectedItem.Value + " " + LaboratorianGV.SelectedRow.Cells[7].Text);
+
+            if (TestNameTb.Value == "")
+            {
+                key = 0;
+            }
+            else
+            {
+                key = Convert.ToInt32(LabTestGV.SelectedRow.Cells[1].Text);
+            }
+        }
+
+        protected void EditBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string TestName = TestNameTb.Value.ToString();
+                string TestCost = TestCostTb.Value.ToString();
+                //Response.Write(RName);
+                string Query = "update LabTestTbl set TestName = '{0}',TestCost = {1} where TestId = {2}";
+                Query = string.Format(Query,TestName,TestCost, LabTestGV.SelectedRow.Cells[1].Text);
+
+                con.SetDatas(Query);
+                ErrMsg.Text = "Test Updated!!!";
+                showTests();
+                TestNameTb.Value = "";
+                TestCostTb.Value = "";
+              
+
+            }
+            catch (Exception ex)
+            {
+                ErrMsg.Text = ex.Message;
+            }
+        }
+
+        protected void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (TestNameTb.Value == "")
+                {
+
+                    ErrMsg.Text = "Select a Test";
+                }
+                else
+                {
+                    string query = "delete from LabTestTbl where TestId={0}";
+                    query = string.Format(query, LabTestGV.SelectedRow.Cells[1].Text);
+                    con.SetDatas(query);
+                    showTests();
+                    ErrMsg.Text = "Lab Test Deleted!!!";
+                    key = 0;
+                    TestNameTb.Value = "";
+                    TestCostTb.Value = "";
+                   
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ErrMsg.Text = ex.Message;
+            }
+        }
+    }
+}
